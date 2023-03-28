bf29de58cf89a49e6e9fea6cc0b4e0436b62d446
wellington <wrogeriowrs@gmail.com>
2023-03-28 17:34:01 -0300

    Add project files.

---
+++ WRogerio.Tools.ServiceBus/SendMessage.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace WRogerio.Tools.ServiceBus
+{
+    public class SendMessage
+    {
+        /// <summary>
+        /// Método usado para enviar mensagem para um tópico no Azure Service Bus
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <param name="topicName"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public async Task<bool> SendMessageToTopic(string connectionString, string topicName, object message)
+        {
+            await using ServiceBusClient client = new ServiceBusClient(connectionString);
+            ServiceBusSender senderObj = client.CreateSender(topicName);
+
+            ServiceBusMessage messageObg = new ServiceBusMessage(message.ToString());
+            try
+            {
+                await senderObj.SendMessageAsync(messageObg);
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
